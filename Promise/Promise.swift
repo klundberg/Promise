9c5761f896724dc3114485444bb9cebb03e3d8df
Kevin Lundberg <klundberg@wayfair.com>
2019-06-24 01:36:17 -0400

    Use Result instead of custom enum for state

--- Promise/Promise.swift
+++ Promise/Promise.swift
@@ -7,18 +7,27 @@
 //
 
 import Foundation
-#if os(Linux)
-    import Dispatch
-#endif
+import Dispatch
 
 public protocol ExecutionContext {
     func execute(_ work: @escaping () -> Void)
 }
 
 extension DispatchQueue: ExecutionContext {
-
     public func execute(_ work: @escaping () -> Void) {
         self.async(execute: work)
+    }
+}
+
+private extension Result {
+    var value: Success? {
+        guard case let .success(value) = self else { return nil }
+        return value
+    }
+
+    var error: Failure? {
+        guard case let .failure(error) = self else { return nil }
+        return error
     }
 }
 
@@ -64,89 +73,22 @@ struct Callback<Value> {
     }
 }
 
-enum State<Value>: CustomStringConvertible {
-
-    /// The promise has not completed yet.
-    /// Will transition to either the `fulfilled` or `rejected` state.
-    case pending
-
-    /// The promise now has a value.
-    /// Will not transition to any other state.
-    case fulfilled(value: Value)
-
-    /// The promise failed with the included error.
-    /// Will not transition to any other state.
-    case rejected(error: Error)
-
-
-    var isPending: Bool {
-        if case .pending = self {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    var isFulfilled: Bool {
-        if case .fulfilled = self {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    var isRejected: Bool {
-        if case .rejected = self {
-            return true
-        } else {
-            return false
-        }
-    }
-    
-    var value: Value? {
-        if case let .fulfilled(value) = self {
-            return value
-        }
-        return nil
-    }
-    
-    var error: Error? {
-        if case let .rejected(error) = self {
-            return error
-        }
-        return nil
-    }
-
-
-    var description: String {
-        switch self {
-        case .fulfilled(let value):
-            return "Fulfilled (\(value))"
-        case .rejected(let error):
-            return "Rejected (\(error))"
-        case .pending:
-            return "Pending"
-        }
-    }
-}
-
-
 public final class Promise<Value> {
     
-    private var state: State<Value>
+    private var state: Result<Value, Error>?
     private let lockQueue = DispatchQueue(label: "promise_lock_queue", qos: .userInitiated)
     private var callbacks: [Callback<Value>] = []
     
     public init() {
-        state = .pending
+        state = nil
     }
     
     public init(value: Value) {
-        state = .fulfilled(value: value)
+        state = .success(value)
     }
     
     public init(error: Error) {
-        state = .rejected(error: error)
+        state = .failure(error)
     }
     
     public convenience init(queue: DispatchQueue = DispatchQueue.global(qos: .userInitiated), work: @escaping (_ fulfill: @escaping (Value) -> Void, _ reject: @escaping (Error) -> Void) throws -> Void) {
@@ -219,11 +161,11 @@ public final class Promise<Value> {
     }
     
     public func reject(_ error: Error) {
-        updateState(.rejected(error: error))
+        updateState(.failure(error))
     }
     
     public func fulfill(_ value: Value) {
-        updateState(.fulfilled(value: value))
+        updateState(.success(value))
     }
     
     public var isPending: Bool {
@@ -240,17 +182,17 @@ public final class Promise<Value> {
     
     public var value: Value? {
         return lockQueue.sync(execute: {
-            return self.state.value
+            return self.state?.value
         })
     }
     
     public var error: Error? {
         return lockQueue.sync(execute: {
-            return self.state.error
+            return self.state?.error
         })
     }
     
-    private func updateState(_ state: State<Value>) {
+    private func updateState(_ state: Result<Value, Error>) {
         guard self.isPending else { return }
         lockQueue.sync(execute: {
             self.state = state
@@ -270,17 +212,17 @@ public final class Promise<Value> {
     
     private func fireCallbacksIfCompleted() {
         lockQueue.async(execute: {
-            guard let callback = self.callbacks.first, !self.state.isPending else {
+            guard let callback = self.callbacks.first, self.state != nil else {
                 return
             }
             self.callbacks.removeFirst()
 
             switch self.state {
-            case let .fulfilled(value):
+            case let .success(value)?:
                 callback.executionContext.execute {
                     callback.callFulfill(value)
                 }
-            case let .rejected(error):
+            case let .failure(error)?:
                 callback.executionContext.execute {
                     callback.callReject(error)
                 }
